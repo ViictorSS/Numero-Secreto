473721c5fa28f486a271163b9d98ecbf02618198
ViictorSS <v.souza31@outlook.com>
2024-08-08 11:23:54 -0300

    alterei o limite e o numero secreto

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroSecreto = parseInt(Math.random() * 55 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt('Escolha um número entre 1 e 55');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
